ad53f2dc7f8c610f22a560646438085bf11eb280
Acty101 <junkit.work@gmail.com>
2023-10-28 15:20:36 -0400

    combined detection model and process img

--- app.py
+++ app.py
@@ -1,7 +1,36 @@
-from flask import Flask
+from flask import Flask, request
+from yolo_model import DetectModel
+from shared_utils import generate_file_from_output
+import os
+
+# to allow ultralytics to load truncated images
+from PIL import ImageFile
+
+ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+INFERENCE_URL = f"/upload-image"
+
+
+@app.route(INFERENCE_URL, methods=["POST"])
+def predict():
+    if request.method != "POST":
+        return
+    
+    # generate img path
+    content = request.get_json()
+    b64_img = content["photo"]
+    img_path = generate_file_from_output(b64_img)
+
+    # create model and run inference
+    config_folder = f"./config"
+    detection_model = DetectModel(
+        os.path.join(config_folder, "best.pt"), os.path.join(config_folder, "taco.yaml")
+    )
+    result = detection_model.predict(img_path=img_path)
+    return result
+    # generate supercategories using LangChain
+
+
+    # generate 
